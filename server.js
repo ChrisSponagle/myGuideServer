be76ce653f51ec3c56d7f0bd090fec250b33c0fd
Chris Sponagle <Chriss.tools@keysupreme.com>
2019-06-10 05:33:24 +0900

    sending photo from server

--- server.js
+++ server.js
@@ -40,12 +40,12 @@ app.post('/siteinfo', (req, res) => {
 
     pool.query(getSiteInfo, (error, result) => {
         if (error) throw error;
-        var photo = './static/photos/' + result.photo
+        var staticPhoto = "<Image src='./static/photos/" + result.photo + "' />"
         var audio = './static/audio/' + result.audio
-        result.photo = "TEST"
+        result.photo = staticPhoto
         result.audio = audio
         console.log("Site Info:", result)
-        res.send(result);
+        res.send(result, staticPhoto);
             
     });
 });
